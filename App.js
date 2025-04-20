1c19bb57f26121e06c62642ce256137b396db1c3
Eraj <122352207+ErajCS@users.noreply.github.com>
2025-04-20 16:52:50 +0500

    Update App.js

--- App.js
+++ App.js
@@ -6,10 +6,17 @@ import Header from './Header';
 import Footer from './Footer';
 import SignupScreen from './signup';  // Ensure the path is correct
 import OtpScreen from './otp';    // Ensure the path is correct
+import CartPage from './cartPage';
+import ItemModal from './ItemModal';
+import EmptyCart from './emptycart';
+import CategoryItems from './CategoryItems';
+import { CartProvider } from './CartContext';
+
 
 const App = () => {
   return (
     <Router>
+      <CartProvider>
       <Header />
       <Routes>
         {/* Default route, shows SweetTreats Home Screen */}
@@ -17,14 +24,21 @@ const App = () => {
         
         {/* Route for Signup screen */}
         <Route path="/signup" element={<SignupScreen />} />
+        <Route path="/" element={<SweetTreats />} />  
         
         {/* Route for OTP screen, after sign up */}
         <Route path="/otp" element={<OtpScreen />} />
         
         {/* Route for Menu screen after logging in */}
         <Route path="/Menu" element={<MenuComponent />} />
+        <Route path="/CategoryItems" element={<CategoryItems />} />
+
+        <Route path="/cartPage" element={<CartPage />} />
+        <Route path="/emptycart" element={<EmptyCart />} />
+        <Route path="/ItemModal" element={<ItemModal />} />
       </Routes>
       <Footer />
+      </CartProvider>
     </Router>
   );
 };
